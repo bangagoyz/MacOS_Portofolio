feabbba830d6f8039a5135a7dfee5ef747604ba5
bangagoyz <yogapermanabudi@gmail.com>
2025-12-21 03:29:28 +0700

    fix potential issues

--- src/components/welcome.jsx
+++ src/components/welcome.jsx
@@ -17,7 +17,7 @@ const renderText = (text, className, baseWeight=400) => {
 }
 
 const setupTextHover = (container, type) => {
-    if (!container)return;
+    if (!container)return() => {};
 
     const letters = container.querySelectorAll('span');
     const {min, max, default: base} = FONT_WEIGHT[type];
@@ -81,7 +81,7 @@ const Welcome = () => {
         </h1>
 
         <div className="small-screen">
-            <p>this Portofolio is designed for desktop/tabled screens only.</p>
+            <p>this Portofolio is designed for desktop/tablet screens only.</p>
         </div>
     </section>
 }
