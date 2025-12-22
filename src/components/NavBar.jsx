277c3a4e374a41f305fcd6f14365f26b9a8a1f20
bangagoyz <yogapermanabudi@gmail.com>
2025-12-22 16:42:06 +0700

    implement resume window feature

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,37 +1,38 @@
 import dayjs from "dayjs";
 import { navIcons, navLinks } from "#constants";
-
+import useWindowStore from "#store/window";
 
 const Navbar = () => {
-    return (
-        <nav>
-            <div>
-                <img src="/images/logo.svg" alt="logo"/> 
-                <p className="font-bold">Yoga's Portofolio</p>
+  const { openWindow } = useWindowStore();
 
-                <ul>
-                    {navLinks.map(({id, name}) => (
-                        <li key={id}>
-                            <p>{name}</p>
-                        </li>
-                    ))}
-                </ul>
-            </div>
+  return (
+    <nav>
+      <div>
+        <img src="/images/logo.svg" alt="logo" />
+        <p className="font-bold">Yoga's Portofolio</p>
 
-            <div>
-                <ul>
-                    {navIcons.map(({id, img}) => (
-                        <li key={id}>
-                            <img src={img} className="icon-hover" 
-                            alt={`icon-${id}`} />
-                        </li>
-                    ))}
-                </ul>
+        <ul>
+          {navLinks.map(({ id, name, type }) => (
+            <li key={id} onClick={() => openWindow(type)}>
+              <p>{name}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
 
-                <time>{dayjs().format('ddd, MMM D, YYYY')}</time>
-            </div>
-        </nav>
-    )
+      <div>
+        <ul>
+          {navIcons.map(({ id, img }) => (
+            <li key={id}>
+              <img src={img} className="icon-hover" alt={`icon-${id}`} />
+            </li>
+          ))}
+        </ul>
+
+        <time>{dayjs().format("ddd, MMM D, YYYY")}</time>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
